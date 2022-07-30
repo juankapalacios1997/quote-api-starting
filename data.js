d2c2a6e9d30867f103e1b5df7778318144dee8a4
juankapalacios1997 <jcmaster_97@hotmail.com>
2022-07-29 21:17:59 -0500

    first changes

--- data.js
+++ data.js
@@ -1,95 +1,103 @@
 const quotes = [
   {
+    id: 1,
     quote: 'We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.',
     person: 'Ellen Ullman'
   },
   {
+    id: 2,
     quote: 'The best thing about a boolean is even if you are wrong, you are only off by a bit.',
     person: 'Anonymous'
   },
-  {
+  { 
+    id: 3,
     quote: `If it's a good idea, go ahead and do it. It's much easier to apologize than it is to get permission.`,
     person: 'Grace Hopper'
   },
   {
+    id: 4,
     quote: 'The city’s central computer told you?  R2D2, you know better than to trust a strange computer!',
     person: 'C-3PO'
   },
   {
+    id: 5,
     quote: 'I have always wished for my computer to be as easy to use as my telephone; my wish has come true because I can no longer figure out how to use my telephone.',
     person: 'Bjarne Stroustrup'
   },
   {
+    id: 6,
     quote: 'Understand well as I may, my comprehension can only be an infinitesimal fraction of all I want to understand.',
     person: 'Ada Lovelace'
   },
   {
+    id: 7,
     quote: 'Java is to JavaScript as ham is to hamster.',
     person: 'Jeremy Keith'
   },
   {
+    id: 8,
     quote: `The most dangerous phrase in the language is, "We've always done it this way."`,
     person: 'Grace Hopper'
   },
   {
+    id: 9,
     quote: 'As soon as we started programming, we found to our surprise that it wasn’t as easy to get programs right as we had thought.  Debugging had to be discovered.  I can remember the exact instant when I realized that a large part of my life from then on was going to be spent in finding mistakes in my own programs.',
     person: 'Maurice Wilkes'
   },
   {
+    id: 10,
     quote: 'Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.',
     person: 'Bill Gates'
   },
   {
+    id: 11,
     quote: 'What one programmer can do in one month, two programmers can do in two months.',
     person: 'Fred Brooks'
   },
   {
+    id: 12,
     quote: 'The Internet? Is that thing still around?',
     person: 'Homer Simpson'
   },
   {
+    id: 13,
     quote: 'If you tell me precisely what it is a machine cannot do, then I can always make a machine which will do just that.',
     person: 'Jon von Neumann'
   },
   {
+    id: 14,
     quote: 'If it was shared on Facebook or Twitter, then it must be true.',
     person: 'Confucius'
   },
   {
+    id: 15,
     quote: 'Anything is a dildo if you are brave enough.',
     person: 'Abraham Lincoln'
   },
   {
+    id: 16,
     quote: 'I can fit two fingers in my urethra.',
     person: 'Daniel Radcliff'
   },
   {
+    id: 17,
     quote: 'I you need someone to talk to, you know where to find me.',
     person: 'Wally'
   },
   {
+    id: 18,
     quote: 'I never said half the crap people say I did.',
     person: 'Albert Einsten'
   },
   {
-    quote: 'Give her the D.',
-    person: 'Rene Descartes'
-  },
-  {
-    quote: 'I can´t breath.',
-    person: 'George Floyd'
-  },
-  {
+    id: 19,
     quote: 'I am so tired that if you told me to eat pussy, I would still eat pussy... but tired.',
     person: 'George W. Bush'
   },
   {
+    id: 20,
     quote: 'I don´t want to sound racist, but....',
     person: 'Adolf Hitler'
-  },
-  {
-    quote: 'I someone disagrees with you, just punch ´em in the throat',
-    person: 'Mahatma Gandhi'
   },
 ];
 
